dd773f95f6eef5b71042c695c99d895ba2f3adac
Alina <alex.alina.rusu@gmail.com>
2018-12-03 18:29:22 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+import { createKoaServer } from "routing-controllers"
+import setupDb from './db'
+
+const port = process.env.PORT || 4000
+
+const app = createKoaServer({
+   cors: true,
+   controllers: []
+})
+
+setupDb()
+   .then(_ =>
+      app.listen(port, () => console.log(`Listening on port ${port}`))
+   )
+   .catch(err => console.error(err))
+
