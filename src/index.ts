286dfe1949501dc05f6b2d842288e57aa6ccb019
Alina <alex.alina.rusu@gmail.com>
2018-12-03 22:56:39 +0100

    added User entity and controller, auth middleware, logins controller

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,56 @@
 import 'reflect-metadata'
-import { createKoaServer } from "routing-controllers"
+import { Action, BadRequestError, createKoaServer } from "routing-controllers"
 import setupDb from './db'
+import UserController from './users/controller';
+
+import { verify } from './jwt'
+import User from './users/entity'
+import LoginController from './logins/controller';
 
 const port = process.env.PORT || 4000
 
 const app = createKoaServer({
-   cors: true,
-   controllers: []
+  cors: true,
+  controllers: [
+    UserController,
+    LoginController,
+  ],
+  //
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ')
+
+      try {
+        return !!(token && verify(token)) //{ id: number}
+      }
+      catch (e) {
+        throw new BadRequestError(e)
+      }
+    }
+
+    return false
+  },
+
+  currentUserChecker: async (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ')
+
+      if (token) {
+        const { id } = verify(token) //{ id: number}
+        return User.findOne(id)
+      }
+    }
+    return undefined
+  },
+
+  //
 })
 
 setupDb()
-   .then(_ =>
-      app.listen(port, () => console.log(`Listening on port ${port}`))
-   )
-   .catch(err => console.error(err))
+  .then(_ =>
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+  )
+  .catch(err => console.error(err))
 
