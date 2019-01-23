bfef112bff0dc2980ccc815eb690086d76838720
Alina <alex.alina.rusu@gmail.com>
2019-01-23 21:57:05 +0100

    fixed endpoints to have more validation

--- src/comments/controller.ts
+++ src/comments/controller.ts
@@ -1,7 +1,15 @@
-import { JsonController, Post, Param, Get, Authorized, HttpCode, Body, CurrentUser, BadRequestError, QueryParams } from 'routing-controllers'
+import { JsonController, Post, Get, Authorized, HttpCode, Body, CurrentUser, BadRequestError, Params } from 'routing-controllers'
 import User from '../users/entity';
 import Comment from './entity';
 import Ticket from '../tickets/entity';
+// import SocialEvent from '../events/entity';
+
+const getTicket = async (eventId, ticketId) => {
+  const ticket = await Ticket.findOne(ticketId, { relations: ["socialEvent"] })
+  if(!ticket) throw new BadRequestError(`Ticket does not exist`)
+  if(parseInt(eventId)  !==  ticket.socialEvent.id ) throw new BadRequestError('Event or ticket does not exist')
+  return ticket
+}
 
 @JsonController()
 export default class CommentController {
@@ -11,30 +19,44 @@ export default class CommentController {
   @HttpCode(201)
   async createComment(
     @Body() comment: Comment,
-    @Param('ticketId') ticketId: number,
-    @QueryParams() 
+    @Params() params:any,
     @CurrentUser() user: User
   ) {
+    const ticket = await getTicket(params.eventId, params.ticketId)
     comment.user = user
-    const ticket = await Ticket.findOne(ticketId)
-    if(!ticket) throw new BadRequestError(`Ticket does not exist`)
     comment.ticket = ticket
     return comment.save()
   }
 
   @Get('/events/:eventId([0-9]+)/tickets/:ticketId([0-9]+)/comments/:commentId([0-9]+)')
   async getComment(
-    @Param('commentId') id: number
+    @Params() params: any
   ) {
-    return await Comment.findOne(id, { relations: ["user", "ticket"] })
+    await getTicket(params.eventId, params.ticketId)
+    const comment = await Comment.findOne(params.commentId, { relations: ["ticket"]})
+    if(!comment) throw new BadRequestError(`Comment does not exist`)
+    if(parseInt(params.ticketId) !==  comment.ticket.id ) throw new BadRequestError('Ticket or comment does not exist')
+    return await Comment.findOne(params.commentId, { relations: ["user"] })
   }
 
 
   @Get('/events/:eventId([0-9]+)/tickets/:ticketId([0-9]+)/comments')
-  async getAllComments() {
-    return { comments: await Comment.find({ relations: ["user", "ticket"] }) }
+  async getAllComments(
+    @Params() params: any
+  ) {
+    await getTicket(params.eventId, params.ticketId)
+    const comments = await Comment.find({ 
+      where: { ticket: { id: params.ticketId } },
+      relations: ["ticket"],
+    })
+    if(!comments) throw new BadRequestError(`No comments found`)
+    return { comments: comments.map(
+      comment => {
+        delete comment.ticket
+        return comment
+      } 
+    )}
   }
 
   //add patch for comments
 }
-
--- src/tickets/controller.ts
+++ src/tickets/controller.ts
@@ -16,9 +16,9 @@ export default class TicketController {
     @Param('eventId') eventId: number,
     @CurrentUser() user: User
   ) {
-    ticket.user = user
     const socialEvent = await SocialEvent.findOne(eventId)
     if (!socialEvent) throw new BadRequestError(`Event does not exist`)
+    ticket.user = user
     ticket.socialEvent = socialEvent
     const time = new Date()
     ticket.createdAt = time
@@ -35,12 +35,13 @@ export default class TicketController {
 
     const ticketRiskData = await Ticket.findOne(params.ticketId, { relations: ["user", "user.tickets", "socialEvent", "socialEvent.tickets","comments"] })
     if (!ticketRiskData) throw new BadRequestError(`Ticket does not exist`)
-    if(ticketRiskData.socialEvent.id !== event.id) throw new BadRequestError('Ticket does not exist for this event')
+    if(ticketRiskData.socialEvent.id !== parseInt(params.eventId)) throw new BadRequestError('Ticket does not exist for this event')
     const risk = calculateRisk(ticketRiskData)
 
-    const ticket = await Ticket.findOne(params.ticketId, { relations: ["user", "socialEvent","comments"] })
+    const ticket = await Ticket.findOne(params.ticketId, { relations: ["user", "socialEvent", "comments"] })
     if (!ticket) throw new BadRequestError(`Ticket does not exist`)
     ticket['risk'] = risk
+
     return ticket
   }
 
@@ -49,11 +50,18 @@ export default class TicketController {
   async getAllTickets(
     @Param('eventId') eventId: number,
   ) {
-    const event = await SocialEvent.findOne(eventId)
+    const event = await SocialEvent.findOne(eventId, {  relations: ["tickets"] })
     if (!event) throw new BadRequestError(`Event does not exist`)
-    return await Ticket.find({ relations: ["user", "socialEvent"] })
+    return { tickets: event.tickets }
   }
 
   //add patch for tickets
 }
 
+ // const allTickets = await Ticket.find({ 
+    //   relations: ["user", "socialEvent"],
+    //   where: { socialEvent: {id: eventId}}
+    // })
+    // if (!allTickets) throw new BadRequestError(`No tickets found`)
+
+    // return { tickets: allTickets.filter(ticket => ticket.socialEvent.id === eventId) }
